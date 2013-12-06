1303825f055081e7ffb6bc9e8c077688aa7e8d32
Markus Bohl <markus.bohl@gmail.com>
2013-12-06 15:38:10 +0100

    A section with start and end points.

---
+++ approximate.search/src/main/java/entity/Section.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Section {
+
+	private final int startIndex;
+	private final int endIndex;
+
+	public Section(final int startIndex, final int endIndex) {
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
+	}
+
+	public int getStartIndex() {
+		return startIndex;
+	}
+
+	public int getEndIndex() {
+		return endIndex;
+	}
+}
