8484871c48e4db67acfc68437213c90cd50173a2
Markus Bohl <markus.bohl@gmail.com>
2013-12-20 23:15:33 +0100

    Forgot Inject annotation.

--- approximate.search/src/main/java/compression/CompressedSequenceBufferedWriter.java
+++ approximate.search/src/main/java/compression/CompressedSequenceBufferedWriter.java
@@ -4,10 +4,12 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 public class CompressedSequenceBufferedWriter extends BufferedWriter {
 
+	@Inject
 	CompressedSequenceBufferedWriter(@Named("compressed.sequence.file.path") final String fileName) throws IOException {
 		super(new FileWriter(fileName));
 	}
