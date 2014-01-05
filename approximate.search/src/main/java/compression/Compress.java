5e32787e8ffd5e500af3be7feff810ee54eb0f55
Markus Bohl <markus.bohl@gmail.com>
2014-01-05 01:05:03 +0100

    Simple sys out of time for debugging purposes.

--- approximate.search/src/main/java/compression/Compress.java
+++ approximate.search/src/main/java/compression/Compress.java
@@ -2,6 +2,8 @@ package compression;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -14,6 +16,7 @@ import common.preparation.StringProviderFactory;
 public class Compress {
 
 	public static void main(final String[] args) throws IOException {
+		System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()));
 		final Injector injector = Guice.createInjector(new CompressionModule());
 		final StringProviderFactory stringProviderFactory = getStringProviderFactory(injector);
 		final String referenceFilePath = getReferenceFilePath(injector);
@@ -21,16 +24,19 @@ public class Compress {
 		final ReferenceIndexStructure indexStructure = getReferenceIndexStructure(injector);
 		indexStructure.init(referenceSequence);
 		System.out.println("### index structure initialized ###");
+		System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()));
 
 		final ReferentialCompressionAlgorithm compressionAlgorithm = getCompressionAlgorithm(injector);
 		final String sequenceToCompressFilePath = getSequenceToCompressFilePath(injector);
 		final String sequenceToCompress = getFastaContentFromFile(sequenceToCompressFilePath, stringProviderFactory);
 		final String compressedResult = compressionAlgorithm.compress(sequenceToCompress);
 		System.out.println("### sequence compressed ###");
+		System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()));
 		final BufferedWriter bufferedWriter = getCompressedSequenceWriter(injector);
 		bufferedWriter.write(compressedResult);
 		bufferedWriter.close();
 		System.out.println("### compressed sequence stored ###");
+		System.out.println(SimpleDateFormat.getTimeInstance().format(new Date()));
 	}
 
 	private static StringProviderFactory getStringProviderFactory(final Injector injector) {
